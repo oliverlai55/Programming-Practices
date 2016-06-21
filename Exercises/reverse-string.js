ea8aa8b6177db0590427127c3f35157fc7691524
Oliver Lai <oliverlai55@gmail.com>
2016-06-21 15:46:45 -0400

    Add practice question inside reverse-string.js

---
+++ Exercises/reverse-string.js
@@ -0,0 +1,15 @@
+// Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+
+// Input:"coderbyte"
+// Output:"etybredoc"
+//
+// Input:"I Love Code"
+// Output:"edoC evoL I"
+
+function FirstReverse(str) {
+
+  
+  return str;
+}
+
+FirstReverse(readline());
