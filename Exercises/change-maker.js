3ce0cee83d0c11494630ca0af458caa9dce243c4
Oliver Lai <oliverlai55@gmail.com>
2016-06-28 16:00:09 -0400

    Provide solution for the change-maker question
    I am making all numbers in the function as integers due to problems with floating-point arithmetic in JavaScript

--- Exercises/change-maker.js
+++ Exercises/change-maker.js
@@ -1,1 +1,33 @@
 //Create a change maker that takes in an amount and returns how many dollars, quarters, dimes, nickels, and pennies are required to equal the amount
+
+var dollarCount = 0;
+var quarterCount = 0;
+var dimeCount = 0;
+var nickelCount = 0;
+var pennyCount = 0;
+var tempAmount;
+
+var changeMaker = function(amount) {
+  tempAmount = amount;
+  amount = amount * 100;
+
+  dollarCount = Math.floor(amount / 100)
+  amount = amount % 100;
+
+  quarterCount =  Math.floor(amount / 25);
+  amount = amount % 25;
+
+  dimeCount = Math.floor(amount / 10);
+  amount % 10;
+
+  nickelCount = Math.floor(amount / 5);
+  amount % 5;
+
+  pennyCount = Math.floor(amount / 1);
+  amount % 1;
+
+  return tempAmount + " will give you " + dollarCount + " dollar bills " + ", " + quarterCount + " quarters, " + dimeCount + " dimes, " + nickelCount + " nickels, " + "and " + pennyCount + " pennies";
+}
+
+//Test
+changeMaker(825.03);
