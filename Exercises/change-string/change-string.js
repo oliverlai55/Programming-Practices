7675db2a24566d9368fa7e3acb434993157e595f
Oliver Lai <oliverlai55@gmail.com>
2016-07-19 00:48:55 -0400

    Add new coding question

---
+++ Exercises/change-string/change-string.js
@@ -0,0 +1,5 @@
+Create a web page that allows a user to input a string, select a text transformation (listed below),
+and click a button to display the output in a second text field.
+Text transformation options for users:
+reverse the input, change to uppercase, change to lowercase, remove vowels,
+replace vowels with numbers, replace certain characters with emojis
