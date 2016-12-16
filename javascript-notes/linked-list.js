0c17ea27f9bedee38b2d30e33115fe5a0b52ed16
oliverlai55 <oliverlai55@gmail.com>
2016-12-16 00:22:27 -0500

    Add insert function

--- javascript-notes/linked-list.js
+++ javascript-notes/linked-list.js
@@ -31,7 +31,36 @@ function LinkedList() {
     length++;
   };
 
-  this.insert = function(position, element) {};
+  this.insert = function(position, element) {
+
+    //check for out of bounds values
+    if (position >= 0 && position <= length) {
+      var node = new Node(element),
+        current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) {
+
+        node.next = current;
+        head = node;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+
+      length++;
+
+      return true;
+
+  } else {
+    return false;
+  }
+};
 
   this.removeAt = function(position) {
 
