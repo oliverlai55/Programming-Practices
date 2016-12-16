f63703d89ba1f8aee97c0147baeaf4b85230abb0
oliverlai55 <oliverlai55@gmail.com>
2016-12-15 22:18:26 -0500

    Add notes

--- javascript-notes/linked-list.js
+++ javascript-notes/linked-list.js
@@ -10,7 +10,27 @@ function LinkedList() {
   var length = 0;
   var head = null;
 
-  this.append = function(element) {};
+  this.append = function(element) {
+    var node = new Node(element),
+    current;
+
+    if (head === null) {
+      head = node;
+    } else {
+      current = head;
+
+      //loop the list until find last item
+      while (current.next) {
+        current = current.next;
+      }
+
+      //get last item and assisn next to node to make the link
+      current.next = node;
+    }
+
+    length++;
+  };
+  
   this.insert = function(position, element) {};
   this.removeAt = function(position) {};
   this.remove = function(element) {};
