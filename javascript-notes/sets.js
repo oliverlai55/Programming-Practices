370fd54768dc6c7490ba6ad96a2d7018b4ad5c10
oliverlai55 <oliverlai55@gmail.com>
2016-12-20 13:34:29 -0500

    Add functions to set

--- javascript-notes/sets.js
+++ javascript-notes/sets.js
@@ -3,5 +3,21 @@ function Set() {
 
   this.has = function(value) {
     return items.hasOwnProperty(value);
-  }
+  };
+
+  this.add = function(value) {
+    if (!this.has(value)) { //make sure value doesn't currently exist
+      items[value] = value;
+      return true;
+    }
+    return false;
+  };
+
+  this.remove = function(value) {
+    if (this.has(value)) {
+      delete items[value];
+      return true
+    }
+    return false;
+  };
 };
