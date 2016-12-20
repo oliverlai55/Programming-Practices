a3b0ec61340968656fba1330e7c9e8638cea1692
oliverlai55 <oliverlai55@gmail.com>
2016-12-20 11:49:00 -0500

    Add removeAt function

--- javascript-notes/linked-list.js
+++ javascript-notes/linked-list.js
@@ -155,7 +155,7 @@ function DoublyLinkedList() {
   var tail = null;
 
   this.insert = function(position, element) {
- 
+
     //check for out of bounds values
     if (position >= 0 && position <= length) {
       var node = new Node(element),
@@ -196,4 +196,50 @@ function DoublyLinkedList() {
       return false;
     }
   }
+
+
+  this.removeAt = function(position) {
+
+    //look for out of bounds values
+    if (position > -1 && position < length) {
+      var current = head,
+          previous,
+          index = 0;
+
+      // removing first item
+      if (position === 0) {
+        head = current.next;
+
+        //if there is only one item, update tail
+        if (length === 1) {
+          tail = null;
+        } else {
+          head.prev = null;
+        }
+      } else if (position === length-1) {
+        current = tail;
+        tail = current.prev;
+        tail.next = null;
+      } else {
+
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+
+        //link previous with current's next - skip it
+        previous.next = current.next;
+        current.next.prev = previous;
+      }
+
+      length--;
+
+      return current.element;
+
+    } else {
+      return null;
+    }
+  };
+
+  
 };
