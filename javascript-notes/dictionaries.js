9f81665414a80941ad2b9f5fecfdd7b91d22f248
oliverlai55 <oliverlai55@gmail.com>
2016-12-20 19:34:38 -0500

    Add remove method

--- javascript-notes/dictionaries.js
+++ javascript-notes/dictionaries.js
@@ -1,4 +1,20 @@
 
 function Dictionary() {
   var items = {};
+
+  this.has = function(key) {
+    return key in items;
+  };
+
+  this.set = function(key, value) {
+    items[key] = value;
+  };
+
+  this.remove = function(key) {
+    if (this.has(key)) {
+      delete items[key];
+      return true;
+    }
+    return false;
+  };
 }
