c09e1df6be677e76730beadde04a99a807e44582
Oliver Lai <oliverlai55@gmail.com>
2016-12-27 12:17:05 -0500

    Add notes on hash table

--- javascript-notes/hash-table.js
+++ javascript-notes/hash-table.js
@@ -9,5 +9,29 @@ function HashTable() {
     return hash % 37;
   };
 
-  
+  this.put = function (key, value) {
+    var position = loseloseHashCode(key);
+    console.log(position + ' - ' + key);
+    table[position] = value;
+  };
+
+  this.get = function (key) {
+    return table[loseloseHashCode(key)];
+  };
+
+  this.remove = function(key) {
+    table[loseloseHashCode(key)] = undefined;
+  }
+
+  this.print = function() {
+    for (var i = 0; i < table.length; ++i) {
+      if (table[i] !== undefined) {
+        console.log(i + ": " + table[i]);
+      }
+    }
+  };
 }
+
+var hash = new HashTable();
+hash.put('Gandalf', 'gandalf@email.com');
+hash.put('John', 'johnsnow@emial.com');
