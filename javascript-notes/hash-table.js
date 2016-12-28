a40b665a9d1803030bf714a7268d21ac651d6147
Oliver Lai <oliverlai55@gmail.com>
2016-12-28 16:28:48 -0500

    Add separate chaining notes

--- javascript-notes/hash-table.js
+++ javascript-notes/hash-table.js
@@ -35,3 +35,66 @@ function HashTable() {
 var hash = new HashTable();
 hash.put('Gandalf', 'gandalf@email.com');
 hash.put('John', 'johnsnow@emial.com');
+
+//Separate Chaining
+var ValuePair = function(key, value) {
+  this.key = key;
+  this.value = value;
+
+  this.toString = function() {
+    return '['  this.key + ' - ' + this.value + ']';
+  }
+
+  this.get = function(key) {
+    var position = loseloseHashCode(key);
+
+    if (table[position] !== undefined) {
+
+      //iterate linked list to find key/value
+      var current = table[position].getHead();
+
+      while (current.next) {
+        if (current.element.key === key) {
+          return current.element.value;
+        }
+        current = current.next;
+      }
+
+      // check in case first or last element
+      if (current.element.key === key) {
+        return current.element.value;
+      }
+    }
+    return undefined;
+  };
+
+  this.remove = function(key) {
+    var position = loseloseHashCode(key);
+
+    if (table[position] !== undefined) {
+
+      var current = table[position].getHead();
+      while (current.next) {
+        if (current.element.key === key) {
+          table[position].remove(current.element);
+          if (table[position].isEmpty() {
+            table[position] = undefined;
+          }
+          return true;
+        }
+        current = current.next;
+      }
+
+      //check in case first or last element
+      if (current.element.key === key) {
+        table[position].remove(current.element);
+        if (table[position].isEmpty()) {
+          table[position] = undefined;
+        }
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
