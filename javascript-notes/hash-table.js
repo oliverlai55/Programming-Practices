87c41bed6bdbb77e9bb3062aa793e4baeeb64b0a
Oliver Lai <oliverlai55@gmail.com>
2016-12-28 17:40:50 -0500

    Add put and get method

--- javascript-notes/hash-table.js
+++ javascript-notes/hash-table.js
@@ -99,4 +99,36 @@ var ValuePair = function(key, value) {
   }
 };
 
-//Linear Probing 
+//Linear Probing
+this.put = function(key, value) {
+  var position = loseloseHashCode(key);
+
+  if (table[position] == undefined) {
+    table[position] = new ValuePair(key, value);
+  } else {
+    var index = ++position;
+    while (table[index] != undefined) {
+      index++;
+    }
+    table[index] = new ValuePair(key, value);
+  }
+};
+
+this.get = function(key) {
+  var position = loseloseHashCode(key);
+
+  if (table[position] !== undefined) {
+    if (table[position].key === key) {
+      return table[position].value;
+    } else {
+      var index = ++position;
+      while (table[index] === undefined || table[index].key !== key) {
+        index++;
+      }
+      if (table[index].key === key) {
+        return table[index].value;
+      }
+    }
+  }
+  return undefined;
+};
