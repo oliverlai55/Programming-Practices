e27582be838992fae776954f402210b49c88df0a
Oliver Lai <oliverlai55@gmail.com>
2016-12-28 17:52:33 -0500

    Add djb2HashCode notes

--- javascript-notes/hash-table.js
+++ javascript-notes/hash-table.js
@@ -132,3 +132,12 @@ this.get = function(key) {
   }
   return undefined;
 };
+
+
+var djb2HashCode = function(key) {
+  var hash = 5381; //1
+  for (var i=0; i<key.length; i++) { //2
+    hash = hash * 33 + key.charCodeAt(i); //3
+  }
+  return hash % 1013; //4
+};
