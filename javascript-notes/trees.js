32a94f6c9399344e7ad692af11efb17178cbc4a2
Oliver Lai <oliverlai55@gmail.com>
2016-12-29 13:04:13 -0500

    Add binary search tree code

--- javascript-notes/trees.js
+++ javascript-notes/trees.js
@@ -8,4 +8,31 @@ function BinarySearchTree() {
   };
 
   var root = null;
+
+  this.insert = function(key) {
+    var newNode = new Node(key); //1
+
+    if (root === null) { //2
+      root = newNode;
+    } else {
+      insertNode(root, newNode); //3
+    }
+
+
+    var insertNode = function(node, newNode) {
+      if (newNode.key < node.key) { //4
+          if (node.left === null) {//5
+            node.left = newNode; //6
+          } else {
+            insertNode(node.left, newNode); //7
+          }
+      } else {
+          if (node.right === null) { //8
+            node.right = newNode; //9
+          } else {
+            insertNode(node.right, newNode); //10
+          }
+      }
+    };
+  };
 }
