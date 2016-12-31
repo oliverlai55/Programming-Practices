86f925cb46e2f7d63b127401ad90bd603bddc6a3
oliverlai55 <oliverlai55@gmail.com>
2016-12-31 16:01:22 -0500

    Add notes

--- javascript-notes/binary-search-tree.js
+++ javascript-notes/binary-search-tree.js
@@ -108,6 +108,80 @@ BinarySearchTree.prototype = {
 
   remove: function(value) {
 
+    var found = false,
+        parent = null,
+        current = this._root,
+        childCount,
+        replacement,
+        replacementParent;
+
+    //make sure there's a node to search
+    while(!found && current){
+
+      //if the value is less than the current node's , go left
+      if (value < current.value) {
+        parent = current;
+        current = current.left;
+      //if the value is greater than the current node's, go right
+      } else if (value > current.value) {
+          parent = current;
+          current = current.right;
+
+      //values are equal, found it!
+      } else {
+          found = true;
+      }
+    }
+
+    //only proceed if the node was found
+    if (found) {
+
+      //figure out how many children
+      childCount = (current.left !== null ? 1 : 0) +
+                   (current.right !== null ? 1: 0);
+
+      //special case: the value is at the root
+      if (current === this._root) {
+        switch(childCount) {
+
+          //no children, just erase the root
+          case 0:
+            this._root = null;
+            break;
+
+          //one child, use one as the root
+          case 1:
+            this._root = (current.right === null ?
+                          current.left : current.right)
+            break;
+
+          //two children
+          case 2:
+
+        }
+      }
+    //non-root value
+    } else {
+
+        switch (childCount) {
+
+          //no children, just remove it from parent
+          case 0:
+          // if the current value is less than its parents,
+          //null out the left pointer
+          if (current.value < parent.value) {
+            parent.left = null;
+
+          //if the current value is greater than its
+          //parents, null out the right pointer
+          } else {
+            parent.right = null;
+          }
+          break;
+
+        //one child, just reassign to parent
+        }
+    }
   },
 
   size: function() {
