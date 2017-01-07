3b053da33bfddc81412c9076112a03891f8c8cfb
oliverlai55 <oliverlai55@gmail.com>
2017-01-07 18:04:35 -0500

    Add forEach example

---
+++ javascript-notes/ES6/Array_Helper/forEach.js
@@ -0,0 +1,63 @@
+var colors = [ 'red', 'blue', 'green' ];
+
+//Using For Loop traditionally
+for (var i=0; i<colors.length; i++) {
+  console.log(colors[i]);
+}
+
+colors.forEach(function(color) {
+  console.log(color);
+});
+
+//Iterator Function
+// It does something, and returns, take in the 2nd element, runs into iterator function
+
+
+// Create an array of numbers
+var numbers = [1,2,3,4,5];
+
+// Create a variable to hold the sum
+var sum = 0;
+
+function adder(number) {
+  sum += number;
+}
+
+// Loop over the array, incrementing the sum variable
+numbers.forEach(function(number) {
+  sum += number;
+});
+
+
+//Or
+numbers.forEach(adder);
+
+// print the sum variable
+console.log(sum);
+
+
+// Example
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+
+    for (var i = 0; i < posts.length; i++) {
+      savePost(posts[i]);
+    }
+}
+
+//Refactor
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+
+    posts.forEach(function(post) {
+      savePost(post);
+    });
+}
