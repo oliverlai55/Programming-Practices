5e92386f71b8b5d6bfbb55c6743083a2d88b2b9e
oliverlai55 <oliverlai55@gmail.com>
2017-01-07 17:50:59 -0500

    Add forEach function

---
+++ javascript-notes/ES6/udemy_es6.js
@@ -0,0 +1,12 @@
+var colors = [ 'red', 'blue', 'green' ];
+
+for (var i=0; i<colors.length; i++) {
+  console.log(colors[i]);
+}
+
+colors.forEach(function(color) {
+  console.log(color);
+});
+
+//Iterator Function
+// It does something, and returns, take in the 2nd element, runs into iterator function
