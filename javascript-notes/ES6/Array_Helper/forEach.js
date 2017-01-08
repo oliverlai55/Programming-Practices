4eca2a6310ade7a250e4e6521ff084861b60e49b
oliverlai55 <oliverlai55@gmail.com>
2017-01-07 19:42:52 -0500

    Add example

--- javascript-notes/ES6/Array_Helper/forEach.js
+++ javascript-notes/ES6/Array_Helper/forEach.js
@@ -61,3 +61,16 @@ function handlePosts() {
       savePost(post);
     });
 }
+
+
+//Calculate area and store in array
+var images = [
+  { height: 10, width: 30 },
+  { height: 20, width: 90 },
+  { height: 54, width: 32 }
+];
+var areas = [];
+
+images.forEach(function(image) {
+    areas.push(image.height * image.width);
+});
