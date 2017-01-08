d9a3cbc63b151f70824559ae464cf78ce3e74762
oliverlai55 <oliverlai55@gmail.com>
2017-01-07 20:11:14 -0500

    Add map notes

--- javascript-notes/ES6/Array_Helper/map.js
+++ javascript-notes/ES6/Array_Helper/map.js
@@ -5,6 +5,40 @@ for (var i = 0; i < numbers.length; i++) {
   doubledNumbers.push(numbers[i] * 2);
 }
 
+
 var doubled = numbers.map(function(number) {
   return number * 2;
+  //remember to include return
+});
+
+
+var cars = [
+  { model: 'Buick', price: 'CHEAP' },
+  { model: 'Camaro', price: 'expensive'}
+];
+
+var prices = cars.map(function(car) {
+  return car.price;
+});
+
+//Example
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var heights = images.map(function(image) {
+   return image.height;
+});
+
+
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map(function(trip) {
+   return trip.distance/trip.time;
 });
