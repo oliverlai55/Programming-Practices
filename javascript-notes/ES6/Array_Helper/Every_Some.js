ef66a07574fe64aed151a26dd77c840925a70dc5
oliverlai55 <oliverlai55@gmail.com>
2017-01-08 23:29:25 -0500

    Add Every and Some notes

---
+++ javascript-notes/ES6/Array_Helper/Every_Some.js
@@ -0,0 +1,72 @@
+var computers = [
+   { name: 'Apple', ram: 24 },
+   { name: 'Compaq', ram: 4},
+   { name: 'Acer', ram: 32 }
+];
+
+var allComputersCanRunProgram = true;
+var onlySomeComputersCanRunProgram = false;
+
+for (var i = 0; i < computers.length; i++) {
+   var computer = computers[i];
+
+   if (computer.ram < 16) {
+      allComputersCanRunProgram = false;
+   } else {
+      onlySomeComputersCanRunProgram = true;
+   }
+}
+
+
+computers.every(function(computer) {
+   return computer.ram > 16;
+});
+// every condition has to meet
+
+computers.some(function(computer) {
+   return computer.ram > 16;
+})
+// only SOME condition has to be met
+
+
+var names = [
+   "Alexandria",
+   "Matthew",
+   "Joe"
+];
+
+names.every(function(name) {
+   return name.length > 4;
+});
+
+names.some(function(name) {
+   return name.length > 4;
+});
+
+
+function Field(value) {
+   this.value = value;
+}
+
+Field.prototype.validate = function() {
+   return this.value.length > 0;
+};
+
+var username = new Field("2cool");
+var password = new Field("my_password");
+var birthdate = new Field("10/10/2010");
+
+username.validate();
+
+var fields = [username, password, birthdate];
+
+var formisValid = fields.every(function(field) {
+   return field.validate();
+});
+
+
+if (formisValid) {
+   //alow user to submit
+} else {
+   //send message warning
+}
--- javascript-notes/ES6/Array_Helper/find.js
+++ javascript-notes/ES6/Array_Helper/find.js
@@ -11,3 +11,43 @@ for (var i=0; i<user.length; i++) {
     user = user[i];
   }
 }
+
+//user find() method
+users.find(function(user) {
+  return user.name === 'Alex';
+});
+
+//it will find the first element that matches, but ONLY the first one if there are more matches
+
+function Car(model) {
+  this.model = model;
+}
+
+var cars = [
+  new Car('Buick'),
+  new Car('Camaro'),
+  new Car('Focus')
+];
+
+cars.find(function(car) {
+  return car.model === 'Focus';
+});
+
+
+var posts = [
+  { id: 1, title: 'New Post'},
+  { id: 2, title: 'Old Post'}
+];
+
+var comment = { postId: 1, content: 'Great Post' };
+
+function postForComment(posts, comment) {
+  return posts.find(function(post) {
+    return post.id === comment.postId;
+  })
+}
+
+postForComment(posts, comment);
+
+//When do we use Find helper??
+//forum or blog, when user clicks, one specific post shows up with post/id
