3aec142a8cd1809755a9c9a6219ecc1736acb251
oliverlai55 <oliverlai55@gmail.com>
2017-01-08 20:14:15 -0500

    Add find method

---
+++ javascript-notes/ES6/Array_Helper/find.js
@@ -0,0 +1,13 @@
+var users = [
+  { name: 'Jill' },
+  { name: 'Alex' },
+  { name: 'Bill' }
+];
+
+var user;
+
+for (var i=0; i<user.length; i++) {
+  if (user[i].name === 'Alex') {
+    user = user[i];
+  }
+}
