14af14edfaf9224e575abd3feea01e0fe6d0ff43
Oliver Lai <oliverlai55@gmail.com>
2017-01-11 18:48:49 -0500

    Add const and let notes

---
+++ javascript-notes/ES6/variables/declarations.js
@@ -0,0 +1,10 @@
+var name = 'Jane';
+var title = 'Software Engineer';
+var hourlyWage = 40;
+
+//ES6
+const name = 'Jane';
+let title = 'Software Engineer';
+let hourlyWage = 40;
+
+//makes it instantly more legible 
