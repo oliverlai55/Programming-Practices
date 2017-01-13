cafe77310138e7a08831e0ecbec6f547128c7b3d
oliverlai55 <oliverlai55@gmail.com>
2017-01-13 01:40:33 -0500

    Add arrow function notes

--- javascript-notes/ES6/Functions/arrow_functions.js
+++ javascript-notes/ES6/Functions/arrow_functions.js
@@ -7,3 +7,33 @@ add(1, 2);
 const add = (a, b) => {
   return a + b;
 }
+
+//if there is just one single expression, "implicait return" of the function
+const add = (a, b) => a + b;
+
+// if we have single argument, ommit params around it
+const double = number => 2 * number;
+
+numbers.map(number => 2 * number );
+
+// with an issue: the this.teamName will cause error cause "this" is not referring to the object
+const team = {
+  members: ['Jane', 'Bill'],
+  teamName: 'Super Squa',
+  teamSummary: function() {
+    return this.members.map(function(member) {
+      return `${member} is on team ${this.teamName}`;
+    });
+  }
+};
+
+//Fix using 'lexical fix' using fat arrow function
+const team = {
+  members: ['Jane', 'Bill'],
+  teamName: 'Super Squa',
+  teamSummary: function() {
+    return this.members.map(member => {
+      return `${member} is on team ${this.teamName}`;
+    });
+  }
+};
