09651c81cf557dcbe130c6b3c60fa717cac959ca
Oliver Lai <oliverlai55@gmail.com>
2017-01-13 14:11:20 -0500

    Add enhanced object literal notes

---
+++ javascript-notes/ES6/Enhanced_Object_Literals/object_literals.js
@@ -0,0 +1,35 @@
+function createBookShop(inventory) {
+   return {
+      inventory: inventory,
+      inventoryValue: function() {
+         return this.inventory.reduce((total, book) => total + book.price, 0);
+      },
+      priceForTitle: function(title) {
+         return this.inventory.find(book => book.title === title).price;
+      }
+   };
+}
+
+const inventory = [
+   { title: "Harry Potter", price: 10 },
+   { title: "Eloquent JS", price: 15 }
+];
+
+const bookShop = createBookShop(inventory);
+
+bookShop.inventoryValue();
+bookShop.priceForTitle('Harry Potter');
+
+
+//Enhanced Object Literal
+function createBookShop(inventory) {
+   return {
+      inventory,
+      inventoryValue() {
+         return this.inventory.reduce((total, book) => total + book.price, 0);
+      },
+      priceForTitle(title) {
+         return this.inventory.find(book => book.title === title).price;
+      }
+   };
+}
