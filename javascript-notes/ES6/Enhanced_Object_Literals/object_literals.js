96e5a61c0e8f1a3c3755a6accb5b3e2bf64e4b1c
Oliver Lai <oliverlai55@gmail.com>
2017-01-13 16:54:38 -0500

    Add object literal notes

--- javascript-notes/ES6/Enhanced_Object_Literals/object_literals.js
+++ javascript-notes/ES6/Enhanced_Object_Literals/object_literals.js
@@ -33,3 +33,17 @@ function createBookShop(inventory) {
       }
    };
 }
+
+
+function saveFile(url, data) {
+   $.ajax({
+      url,
+      data,
+      method: "Post"
+   });
+}
+
+const url = 'http://fileupload.com';
+const data = { color: 'red' };
+
+saveFile(url, data)
