31220d5f376d60f8abfece2ceb970ef55d0fe2e8
oliverlai55 <oliverlai55@gmail.com>
2017-01-15 17:53:27 -0500

    Add default argument notes

--- javascript-notes/ES6/Default_Function_Arguments.js
+++ javascript-notes/ES6/Default_Function_Arguments.js
@@ -6,3 +6,23 @@ makeAjaxRequest('google.com');
 makeAjaxRequest('google.com', 'POST')
 
 // the method argument will be default to 'GET' UNLESS you pass in a specific argument
+
+
+function User(id) {
+  this.id = id;
+}
+
+function generateId() {
+  return Math.random() * 999999;
+}
+
+function createAdminUser() {
+  user.admin = true;
+
+  return user;
+}
+
+createAdminUser(new User(generateId()));
+
+const user = new User(generateId);
+createAdminUser(user);
