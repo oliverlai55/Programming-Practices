4543f0ff9fbfa0fad37c2341e6e865d91ff83bcf
oliverlai55 <oliverlai55@gmail.com>
2017-01-15 18:38:00 -0500

    Add notes

--- javascript-notes/ES6/Rest_Spread_Operator/rest_spread_operator.js
+++ javascript-notes/ES6/Rest_Spread_Operator/rest_spread_operator.js
@@ -14,3 +14,26 @@ funciton addNumbers(...numbers) {
 }
 
 addNumber([1,2,3,4,5]);
+
+
+const defaultColors = ['red', 'green'];
+const userFavoriteColors = ['orange', 'yellow'];
+const fallColors = ['fire red', 'fall orange'];
+
+defaultColors.concat(userFavoriteColors);
+
+//Using Spread
+//Create new array, reference existing array, using spread operator
+// Can add additional info in array
+[ 'green', 'blue', ...fallColors, ...defaultColors, ...userFavoriteColors ];
+
+
+function validateShoppingList(...items) {
+  if (item.indexOf('milk') < 0) {
+    return [ 'milk', ...items ];
+  }
+
+  return items;
+}
+
+validateShoppingList('oranges', 'bread', 'eggs');
