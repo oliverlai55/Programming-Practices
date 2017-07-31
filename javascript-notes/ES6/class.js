74797fb20b91d4438497d9c9534288bf3939a58b
oliverlai55 <oliverlai55@gmail.com>
2017-07-30 23:27:15 -0400

    Add notes on classes

---
+++ javascript-notes/ES6/class.js
@@ -0,0 +1,25 @@
+class Car {
+  constructor({ title }) {
+    this.title = title;
+  }
+
+  drive() {
+    return 'vroom';
+  }
+}
+
+class Toyota extends Car {
+  constructor(options) {
+    super(options); //Car.constructor() used to inherit the methods from the parent class
+    this.color = options.color;
+  }
+
+  honk() {
+    return 'beep';
+  }
+}
+
+const toyota = new Toyota({ color: 'red' });
+
+toyota.honk();
+toyota.drive();
