1b8b8ddf2986e97d0c95f3f7e320cee7f2e7ca2d
oliverlai55 <oliverlai55@gmail.com>
2017-10-29 15:57:07 -0400

    Format comments

--- javascript-notes/udemy-algo/practice.js
+++ javascript-notes/udemy-algo/practice.js
@@ -37,4 +37,6 @@ function harmlessRansomNote (noteText, magazineText) {
 harmlessRansomNote('this is a secret note for you from a secret admier', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited');
 
 // Linear Time complexity
-// the two loops run in O(n) run time. The n in the O(n) represents how many element in the magazine obj. Linear Time Complexity O(n) + O(m) -> O(n + m)
+// the two loops run in O(n) run time.
+// The n in the O(n) represents how many element in the magazine obj.
+// Linear Time Complexity O(n) + O(m) -> O(n + m)
