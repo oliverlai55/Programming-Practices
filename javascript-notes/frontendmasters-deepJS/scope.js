5422bdb4ade3e20f9e3eb3162a3de1c5c60e64ca
oliverlai55 <oliverlai55@gmail.com>
2017-11-28 02:51:49 -0500

    Add notes on scope

--- javascript-notes/frontendmasters-deepJS/scope.js
+++ javascript-notes/frontendmasters-deepJS/scope.js
@@ -1,3 +1,17 @@
+// JS is a compiled language
 // Scope: where to look for things
 // Scope is a compiled time process, programming goes through a compiled step
-// 
+// there are 2 paths, the 1st path sets up lexical scope happens, not during 2nd
+
+var foo = "bar";
+
+function bar() {
+  var foo = "baz";
+}
+
+function baz(foo) {
+  foo = "bam";
+  bam = "yay";
+}
+
+// 1st pass, we look at var declaration, 'formal declaration' put declaration in global scope
