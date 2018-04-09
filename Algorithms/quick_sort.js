e402751b91b68f799f56ba66fd3436f881ade15b
Oliver Lai <oliverlai55@gmail.com>
2018-04-09 18:08:01 -0400

    Add quick sort

--- Algorithms/quick_sort.js
+++ Algorithms/quick_sort.js
@@ -6,6 +6,34 @@ var quick = function(array, left, right) {
   var index; //1  help us separate the subarray with smaller and greater values so we can recursively call the function again
 
   if (array.length > 1) {
-    index = partition(array, left, right);
+    index = partition(array, left, right); //3 obtain the index value as the return value of partition function
+
+    if (left < index - 1) { //4 if a subarray with smaller elements exists, repeat process for the subarray
+      quick (array, left, index - 1);
+    }
+
+    if (index < right) {
+      quick (array, index, right);
+    }
   }
-}
+};
+
+var partition = function(array, left, right) {
+  var pivot = array[Math.floor((right + left) / 2)] // select middle item as pivot
+  var i = left, j = right
+
+  while (i <= j) {
+    while (array[i] < pivot) {
+      i++
+    }
+    while (array[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(array, i, j);
+      i++;
+      j--;
+    }
+  }
+  return 1;
+};
