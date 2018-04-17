0acac20dba9ea646860253a972c84d329213102f
Oliver Lai <oliverlai55@gmail.com>
2018-04-17 18:28:53 -0400

    Add notes

--- DataStructure/check_balanced_brackets.js
+++ DataStructure/check_balanced_brackets.js
@@ -3,6 +3,13 @@
  * @return {boolean}
  */
 
+// Logic:
+// Declare a character stack S
+// Traverse the string expression
+// if the current character is an opening bracket ( or { or [, then push to stack
+// if the current character is a closing bracket ) or } or ], then pop from stack and if the popped character is the matching starting bracket then fine
+// At the end of the traversal, if there is some opening bracket left in stack then the string is “not balanced”. Hence, return false.
+
  const isValid = str => {
    if (str.length <= 1) return false;
 
