0f6aab99a95248f0e23e2d8027caff83b712c831
oliverlai55 <oliverlai55@gmail.com>
2018-05-03 01:19:44 -0400

    Add exercise

--- Exercises/code_wars/findEvenIndex.js
+++ Exercises/code_wars/findEvenIndex.js
@@ -1,4 +1,6 @@
 // You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+// https://www.codewars.com/kata/equal-sides-of-an-array/train/javascript
 function findEvenIndex(arr) {
 	//Code goes here!
 	// leftSum = calculate current leftSum
