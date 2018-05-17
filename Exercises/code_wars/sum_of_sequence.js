76cc06d11a9c627fdd50c40f6de406b140936c98
Oliver Lai <oliverlai55@gmail.com>
2018-05-17 17:16:37 -0400

    Add CW exercise

---
+++ Exercises/code_wars/sum_of_sequence.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/586f6741c66d18c22800010a/train/javascript
+
+const sequenceSum = (begin, end, step) => {
+   if (begin > end) return 0;
+
+   let currentTotal = begin;
+   let addNumber = begin + step;
+
+   for (currentTotal = begin; addNumber <= end; addNumber+=step) {
+      currentTotal += addNumber;
+   }
+
+   return currentTotal;
+ };
+
+sequenceSum(2,2,2) === 2
+sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+sequenceSum(1,5,3) === 5 // 1 + 4
+
+
+while (addNumber <= end) {
+   addNumber = currentTotal + step;
+   currentTotal += addNumber;
+}
