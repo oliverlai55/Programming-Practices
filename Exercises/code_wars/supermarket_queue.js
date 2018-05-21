44113b26afc110981ff209f754aba514eb4055a9
Oliver Lai <oliverlai55@gmail.com>
2018-05-21 14:20:31 -0400

    Add new problem

---
+++ Exercises/code_wars/supermarket_queue.js
@@ -0,0 +1,11 @@
+// http://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
+
+function queueTime(customers, n) {
+  //TODO
+}
+
+
+// queueTime([], 1), 0);
+// Test.assertEquals(queueTime([1,2,3,4], 1), 10);
+// Test.assertEquals(queueTime([2,2,3,3,4,4], 2), 9);
+// Test.assertEquals(queueTime([1,2,3,4,5], 100), 5);
